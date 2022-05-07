5bbd9eee6fc94b6ca974da23a88b0600d78f2226
Ranel Nasybullin <dogjamal@gmail.com>
2022-05-07 18:17:23 +0300

    print

--- Stream/Program.cs
+++ Stream/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mdr = new print();
+            mdr.print_method();
+        }
+    }
+}
---
+++ Stream/print.cs
@@ -0,0 +1,61 @@
+﻿namespace Stream
+{
+    internal class print
+    {
+        internal void print_method()
+        {
+            var mdr = new drive();
+            var mdi = new directories();
+
+            Console.WriteLine("Type P for print Drives");
+            Console.WriteLine("Type F for print info about Directories");
+            Console.WriteLine("*************************************");
+            Console.WriteLine("Type E Exit");
+
+            string response = "";
+            bool exiting = false;
+
+            response = Console.ReadLine();
+
+            while (response != "e" || response != "E")
+            {
+                switch (response)
+                {
+                    case "P":
+                    case "p":
+                        mdr.print_drive();
+                        break;
+
+                    case "F":
+                    case "f":
+                        mdi.print_directory();
+                        break;
+
+                    case "E":
+                    case "e":
+                        Console.WriteLine("Good Bye!");
+                        System.Threading.Thread.Sleep(100);
+                        exiting = true;
+                        break;
+                    default:
+                        Console.WriteLine($"no command for {response} key");
+                        Console.WriteLine("*************************************");
+                        Console.WriteLine("Type P for print Drives");
+                        Console.WriteLine("Type F for print info about Directories");
+                        Console.WriteLine("*************************************");
+                        Console.WriteLine("Type E Exit");
+                        break;
+                }
+
+                if (exiting)
+                {
+                    break;
+                }
+                else
+                {
+                    response = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
